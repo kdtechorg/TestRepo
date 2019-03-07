80b705f2642cffd6d0910108cda61265ec7ccfc4
Deepak Sharma <deepaks@synergy.com>
2019-03-07 15:53:42 +0000

    service A

---
+++ src/main/java/com/kdtech/serviceA/controller/ServiceAController.java
@@ -0,0 +1,29 @@
+package com.kdtech.serviceA.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kdtech.serviceA.model.Rating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class ServiceAController {
+
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId) {return new Rating(movieId, 4);}
+
+	@RequestMapping("/users{userId}")
+	public List<Rating> getUserRating(@PathVariable("userId") String userID)
+	{
+		List<Rating> ratings = Arrays.asList(
+				new Rating("1234" , 4),
+				new Rating("5678" , 4)
+                );
+		return ratings;
+	}
+
+}
